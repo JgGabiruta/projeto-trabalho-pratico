986cf192c955e68cf56643d5769a9e268659b2ac
João Gabriel Soares da Silva Franco <jgssfranco@sga.pucminas.br>
2024-06-13 15:35:38 -0300

    Branch nova de testes do servidor para o vercel adicionada

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,20 @@
-//Json-server
-const jsonServer = require('json-server')
-const { get } = require('lodash')
-const server = jsonServer.create()
-const router = jsonServer.router('./scripts/db.json')
-
-// Para permitir que os dados sejam alterados, altere a linha abaixo
-// colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server está em execução!')
-})
-const texto = document.getElementById('empresaD');
 const empresa = document.getElementById('empresa');
-fetch ('https://localhost:3000/conteudo')
-.then(response => response.json())
-.then(data => {
-  texto.innerHTML = data.empresa
-})
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
+const texto = document.getElementById('empresaD');
+
+fetch('http://localhost:3000/conteudo')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao carregar os dados');
+    }
+    return response.json();
+  })
+  .then(conteudo => {
+    empresa.innerHTML = conteudo.empresa || "Dados não encontrados";
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
+  function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  }
+
---
+++ scripts/server/server.js
@@ -0,0 +1,23 @@
+// JSON - Server Module
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
+);
+
+server.use(router);
+//Listen to port
+server.listen(3000, () => {
+  console.log('JSON Server está em execução!');
+});
+// Export the server api
+module.exports = server;
