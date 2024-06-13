0f852516b8d526b1112604c776e521bf79203793
João Gabriel Soares da Silva Franco <jgssfranco@sga.pucminas.br>
2024-06-13 17:00:46 -0300

    bugfix test

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const empresa = document.getElementById('empresa');
 const texto = document.getElementById('empresaD');
 
-fetch('/api/server')
+fetch('/api/conteudo')
   .then(response => {
     if (!response.ok) {
       throw new Error('Erro ao carregar os dados');
